e720a31db31e0b16c12c00407709b6def9584d08
@Nagaraj_Nagu <nagarajanknkn9@gmail.com>
2025-04-01 20:34:41 +0530

    completed a authentication login

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,16 +1,17 @@
 const authenticationService = require("../services/authenticationService");
+const { HTTP_RESPONSE, HTTP_STATUS_CODE } = require("../utils/httpResponse");
 
 class AuthenticationController {
     async authLogin(req, res) {
         try {
             const authData = await authenticationService.authLogin(req.body);
             res.status(200).json({
-                success: true,
+                status: HTTP_RESPONSE.SUCCESS,
                 message: "Logged in successfully",
                 ...authData
             });
         } catch (err) {
-            res.status(500).json({ success: false, message: err.message });
+            res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json({ success: HTTP_RESPONSE.FAIL, message: err.message });
         }
     }
 }
--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-const { baseUrl } = require('../routes');
-
+const Setting = require('../models/settingModel');
+const { HTTP_STATUS_CODE } = require('../utils/httpResponse');
 
 const excludedPath = [
     'auth/login',
@@ -11,52 +11,57 @@ const excludedPath = [
 
 const authenticate = async (req, res, next) => {
 
-    console.log("req", req);
-    console.log("req", req.path);
-
     try {
         let apiPath = req.path.replace('/api/v1/', '');
         if (excludedPath.includes(apiPath)) {
             return next();
         } else {
             const accessToken = req.headers['authorization'];
-            if (!accessToken) return res.status(401).json({ status: false, message: 'Access Denied' });
-    console.log("accessToken", accessToken);
+            if (!accessToken) return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Access Denied' });
             try {
                 const result = await jwt.verify(accessToken, process.env.JWT_SECRET);
 
                 if (!result) {
-                    return res.status(403).json({ status: 0, message: 'Invalid Token. Please contact an admin' });
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Invalid Token. Please contact an admin' });
                 }
 
-                // const userData = await User.findById(result._id);
-
-                if (userData.isactive) {
-                    return res.status(403).json({ status: 0, message: 'Your account has been blocked. Please contact an admin' });
+                let modelName;
+                if (result.email === process.env.ADMIN_EMAIL) {
+                    modelName = Setting;
+                } else {
+                    // modelName = User;
                 }
 
-                if (!result.password) {
-                    return res.status(403).json({ status: 0, message: 'Password not found in token' });
+                const userData = await modelName.findOne({ _id: result.id }).select("password");
+
+                if (!userData) {
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Please contact an admin' });
                 }
 
-                req.ID = result._id;
+                if (userData?.isactive === "inactive") {
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Your account has been blocked. Please contact an admin' });
+                }
+
+                if (userData?.password !== result.password) {
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Password has been changed' });
+                }
+
+                req.id = result._id;
                 req.email = result.email;
                 req.accountdetails = result;
-                req.role = result.role;
                 return next();
             } catch (error) {
-    console.log("error", error);
-    if (error.name === 'TokenExpiredError') {
-                    return res.status(401).json({ status: false, message: 'Session Expired. Please login again' });
+                if (error.name === 'TokenExpiredError') {
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Session Expired. Please login again' });
                 } else if (error.name === 'JsonWebTokenError') {
-                    return res.status(403).json({ status: false, message: 'Invalid Token. Please login again' });
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Invalid Token. Please login again' });
                 } else {
-                    return res.status(500).json({ status: false, message: 'Internal Server Error' });
+                    return res.status(HTTP_STATUS_CODE.FORBIDDEN).json({ status: HTTP_RESPONSE.FAIL, message: 'Internal Server Error' });
                 }
             }
         }
     } catch (err) {
-        res.status(400).json({ status: false, message: 'Invalid Token' });
+        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).json({ status: HTTP_RESPONSE.FAIL, message: err.message });
     }
 };
 
